3b4bd2be7e7d51686973177e2d5e61e9fb99624a
jiachen0212 <jiachen0212@163.com>
2022-07-13 20:24:23 +0800

    update apple_locate

--- locate/locate.py
+++ locate/locate.py
@@ -83,14 +83,22 @@ def mkdir(save_dir):
 
 def main_fun(js_dir, test_path, train_dir=None, flag=None, roi_vis_path=None):
     ims = os.listdir(test_path)
-    paths = [os.path.join(test_path, a) for a in ims]
+    paths = [os.path.join(test_path, a) for a in ims if '.bmp' in a]
 
-    # 开启 train     
+    # 开启 train: left and right   
     # img = cv2.imread(os.path.join(train_dir, "image.bmp"), 0)
     # five_holes_area = cv2.imread(os.path.join(train_dir, "imac.bmp"), 0)
     # roi = cv2.imread(os.path.join(train_dir, "template.bmp"), 0)
     # train_info = train(img, five_holes_area, roi)
     # with open(os.path.join(train_dir, "train_info.json"), "w") as f:
+    #     json.dump(train_info, f, indent=4)
+    
+    # 开启 train: apple logo   
+    # img = cv2.imread(os.path.join(train_dir, "image.jpg"), 0)
+    # five_holes_area = cv2.imread(os.path.join(train_dir, "apple.jpg"), 0)
+    # roi = cv2.imread(os.path.join(train_dir, "apple_template.jpg"), 0)
+    # train_info = train(img, five_holes_area, roi)
+    # with open(os.path.join(train_dir, "apple.json"), "w") as f:
     #     json.dump(train_info, f, indent=4)
 
 
@@ -101,6 +109,33 @@ def main_fun(js_dir, test_path, train_dir=None, flag=None, roi_vis_path=None):
     elif flag == 'right':
         with open(os.path.join(js_dir, "right.json")) as f:
             train_info = json.load(f)
+    elif flag == 'apple_logo':
+        with open(os.path.join(js_dir, "apple.json")) as f:
+            train_info = json.load(f)
+        ttemp = os.path.join(train_dir, "apple_template.jpg")
+        roi = cv2.imread(ttemp, 0)
+        train_info["roi"] = roi
+        for img_path in paths:
+            im_name = os.path.basename(img_path)
+            img = cv_imread_by_np(img_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            inference_info = inference(img, train_info, verbose='False')
+            _, p2 = inference_info['area_points'][0], inference_info['area_points'][2]
+
+            # cv2.circle(img, (int(p2[0]), int(p2[1])), 10, (0, 255, 0), 20)
+            # cv2.imwrite('./3.jpg', img)
+            print(p2)
+            temp_point = [4697, 10197]
+            r = np.sqrt((temp_point[0]-p2[0])**2 + (temp_point[1]-p2[1])**2)
+            temp_point1 = [4497, 9997]
+            r1 = np.sqrt((temp_point1[0]-p2[0])**2 + (temp_point1[1]-p2[1])**2)
+            print(r, r1)
+            points = [p2] + [temp_point] + [temp_point1]
+            cv2.circle(img, (int(p2[0]), int(p2[1])), int(r), (255, 255, 255), 20)
+            cv2.circle(img, (int(p2[0]), int(p2[1])), int(r1), (255, 255, 255), 20)
+            cv2.imwrite('./3.jpg', img)
+
+            return 
 
     ttemp = os.path.join(train_dir, "template_{}.bmp".format(flag))
     roi = cv2.imread(ttemp, 0)
@@ -125,10 +160,51 @@ def main_fun(js_dir, test_path, train_dir=None, flag=None, roi_vis_path=None):
     return img_roi
 
 
+    # image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    # # 剔除冗余, 把边上的黑色部分剔除, 只剩下干净的物料
+    # image = image[1185:19892, 2500: 8192]
+
+    # # 二值化并找到apple-logo的最小外接圆
+    # otsuThe, maxValue = 0, 255
+    # # otsuThe, dst_Otsu = cv2.threshold(image, otsuThe, maxValue, cv2.THRESH_OTSU)
+    # # print(otsuThe)
+    # _, dst_Otsu = cv2.threshold(image, 0, maxValue, cv2.THRESH_OTSU)
+    # dst_Otsu = cv2.bitwise_not(dst_Otsu)
+    # cv2.imwrite('./1.jpg', dst_Otsu)
+    # contours, _ = cv2.findContours(dst_Otsu, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnt = contours[0]
+
+    # # (x, y), radius = cv2.minEnclosingCircle(cnt)  
+    # # print((x, y), radius)
+    # # # cv2.circle(image, (int(x), int(y)), int(radius), (0, 255, 0), 20)
+    # # cv2.circle(image, (int(x),int(y)), 10, (255,255,255), 10)
+    # # cv2.imwrite('./1.jpg', image)
+    # contours, _ = cv2.findContours(dst_Otsu, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # # 绘制轮廓
+    # cv2.drawContours(dst_Otsu, contours, -1, (128,128,128), 30, lineType=cv2.LINE_AA)
+    # cv2.imwrite('./2.jpg', dst_Otsu)
+
+    # img_path = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate\apple\image.jpg'
+    # image = cv2.imread(img_path)
+    # # temple = image[920:10593, 1560:6553]
+    # temple = image[920:15491, 1560:7537]
+    # cv2.imwrite('./apple_template.jpg', temple)
+
+
 if __name__ == '__main__':
-    js_dir = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate'
-    train_dir = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate\train_dir'
-    test_path = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\data\test_tune\腐蚀点模型测试数据\left'
+    
+    # 左右物料定位
+    # js_dir = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate'
+    # train_dir = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate\train_dir'
+    # test_path = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\data\test_tune\腐蚀点模型测试数据\left'
+    # roi_vis_path = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\data\test_tune\vis_roi'
+    # img_roi = main_fun(js_dir, test_path, train_dir=train_dir, flag='left', roi_vis_path=roi_vis_path)
+
+
+    js_dir = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate\apple'
+    train_dir = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate\apple'
+    test_path = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\codes\locate\apple_test'
     roi_vis_path = r'D:\mac_air_backup\chenjia\Download\Smartmore\2022\DL\kesen\data\test_tune\vis_roi'
-    img_roi = main_fun(js_dir, test_path, train_dir=train_dir, flag='left', roi_vis_path=roi_vis_path)
+    main_fun(js_dir, test_path, train_dir=train_dir, flag='apple_logo', roi_vis_path=None)
+    
     
