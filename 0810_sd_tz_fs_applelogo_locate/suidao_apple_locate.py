7cd198dca69cb4d2e1a431ab4ad112838008cb30
j c <jiachen0212@163.com>
2022-08-11 19:33:11 +0800

    update suidao apple locate

--- 0810_sd_tz_fs_applelogo_locate/suidao_apple_locate.py
+++ 0810_sd_tz_fs_applelogo_locate/suidao_apple_locate.py
@@ -43,12 +43,12 @@ def localize_one_edge(source_image, find_in_vertical=True, thre=None, expend=200
 
 if __name__ == "__main__":
 
-    flag = 1 # 左半图
+    flag = 0 # 左半图
     # flag = 0  # 右半图
 
     if flag == 0:
         # 隧道的右半张子图  [apple-logo少的那半张]
-        sd2_image = cv2.imread(r'D:\work\project\DL\kesen\data\19-2-8.bmp')
+        sd2_image = cv2.imread(r'D:\work\project\DL\kesen\data\19-1-8.bmp')
         sd2_image = cv2.cvtColor(sd2_image, cv2.COLOR_BGR2RGB) 
         sd2_image = cv2.cvtColor(sd2_image, cv2.COLOR_RGB2GRAY)  
         a, b = localize_one_edge(sd2_image, find_in_vertical=True, thre=None, expend=200)
@@ -58,18 +58,19 @@ if __name__ == "__main__":
         d -= 3900
         # a是物料上边界, 固定+1000充分剔除冗余且不至于影响apple-logo;
         # b是物料下边界, 固定-1000充分剔除冗余且不至于影响apple-logo;
-        a += 1000
-        b -= 1000
+        a += 8000
+        b -= 8000
         wuliao_tz = sd2_image[a:b, c:d]
+        cv2.imwrite('./wuliao_sd.jpg', wuliao_tz)
         otsuThe, maxValue = 0, 255  # otsuThe=46
         otsuThe, dst_Otsu = cv2.threshold(wuliao_tz, otsuThe, maxValue, cv2.THRESH_OTSU)
-        
+        _, dst_Otsu = cv2.threshold(wuliao_tz, otsuThe+20, maxValue, cv2.THRESH_OTSU)
         dst_Otsu = cv2.bitwise_not(dst_Otsu)
         # 检测出的apple-logo边上还是有点黑点, so先腐蚀(去除黑点)再膨胀(外扩白像素.)
-        kernel = np.ones((30, 30), dtype=np.uint8)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(30,30))
         dst_Otsu1 = cv2.erode(dst_Otsu, kernel, iterations=1)
-        kernel = np.ones((50, 50), dtype=np.uint8)
-        dst_Otsu2 = cv2.dilate(dst_Otsu1, kernel, 5)  
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(50,50))
+        dst_Otsu2 = cv2.dilate(dst_Otsu1, kernel, 2)  
         # 把abcd添加回去, 得到和原图一样size的apple-mask
         full_mask = np.zeros_like(sd2_image)
         full_mask[a:b, c:d] = dst_Otsu2
