612fcd59359a5772c86d6e3804415c6d43089927
jiachen <jiachen0212@163.com>
2022-08-30 17:25:45 +0800

    Add files via upload

--- pre_data_process.py
+++ pre_data_process.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import cv2
 import os
 import shutil 
@@ -35,6 +35,28 @@ def slim_dirs1(dir_):
             # print(os.path.join(path, im_name), os.path.join(dir_, im_name))
         shutil.rmtree(path)
     
+
+
+def slim_dirs_js(dir_):
+    im_dirs = [os.path.join(dir_, a) for a in os.listdir(dir_)]
+    for im_dir in im_dirs:
+        im_names = [a for a in os.listdir(im_dir) if '.json' in a]
+        for im_name in im_names:
+            shutil.copy(os.path.join(im_dir, im_name), os.path.join(dir_, im_name))  
+        shutil.rmtree(im_dir)
+
+
+def slim_dirs1_js(dir_):
+    # 文件夹下还有ok ng的子名字的, 先剔除ok ng这一层. 
+    dirs = [os.path.join(dir_, a) for a in os.listdir(dir_)]
+    for dir_  in dirs:
+        path = os.path.join(dir_, os.listdir(dir_)[0])
+        ims = [a for a in os.listdir(path) if '.json' in a]
+        for im_name in ims:
+            shutil.copy(os.path.join(path, im_name), os.path.join(dir_, im_name))  
+            # print(os.path.join(path, im_name), os.path.join(dir_, im_name))
+        shutil.rmtree(path)
+
 
 def localize_one_edge(source_image, find_in_vertical=True, thre=None, expend=200):
     
@@ -82,14 +104,47 @@ def generate_im_roi_json(base_dir, roi_json_name):
         json_file.write(json_str)
 
 
+def json_label_check(js_path, defects, defcet_nums):
+    try:
+        data = json.load(open(js_path, 'r'))
+    except:
+        return 0
+
+    if len(data['shapes']) > 0:
+        for cls_ in data['shapes']:
+            def_ = cls_['label'].encode('gbk').decode('utf-8')
+            defcet_nums[defects.index(def_)] += 1
+    return 1
+
+
 if __name__ == "__main__":
 
-    base_dir = r'D:\work\project\DL\kesen\data\20220825 工位4 腐蚀点过漏杀样图\原图'
-    slim_dirs1(base_dir)
-    slim_dirs(base_dir)
+    # for .bmps
+    # base_dir = r'D:\work\project\DL\kesen\data\腐蚀点缺陷样图\过杀\0829\0829'
+    # slim_dirs1(base_dir)
+    # slim_dirs(base_dir)
 
     # 动态生成roi
-    generate_im_roi_json(base_dir, './0825_1_im_roi.json')
+    # generate_im_roi_json(base_dir, './0829_im_roi.json')
+
+
+    # for jss
+    base_dir = r'D:\work\project\DL\kesen\data\20220828_JSON\20220827 工位3&4 缺陷图样\腐蚀点缺陷样图\过杀\原图'
+    slim_dirs1_js(base_dir)
+    slim_dirs_js(base_dir)
+
+
+
+    # defects = [r"发黑", r"堆银", r"残银", r"破损", r"划痕", r"裂片"]
+    # defcet_nums = [0]*len(defects)
+    # json_dir = r'D:\work\project\beijing\Smartmore\2022\DL\vimo算法联动\0829迈维视\data_0829'
+    # js_paths = [os.path.join(json_dir, a) for a in os.listdir(json_dir) if '.json' in a]
+    # for js_path in js_paths:
+    #     json_label_check(js_path, defects, defcet_nums)
+    # a = ''
+    # for ind, b in enumerate(defects):
+    #     a += '{}: {}, '.format(b, defcet_nums[ind])
+    # print(a)
 
 
     
